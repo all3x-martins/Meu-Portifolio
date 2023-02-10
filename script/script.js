89581ed2d90f8adba664b4126297a664eeeb0c8d
A113x <85321713+all3x-martins@users.noreply.github.com>
2023-02-10 12:33:33 -0300

    Atualização total do código
    Mudanças no designer e muitas outras melhorias

--- script/script.js
+++
@@ -1,6 +0,0 @@
-function displayAbout() {
-    var arrow = document.getElementById('animation');
-    arrow.classList.toggle('rotate');
-    var display = document.getElementById('about');
-    display.classList.toggle('active');
-}
